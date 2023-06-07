76db60f26c8b7f0e8905909b4cdb37ec37e9ecfc
dragonrain5761 <dragonrain5761@gmail.com>
2023-06-07 18:19:47 +0900

    공룡게임 CSS, JS파일 수정

--- Js/dinosaur.js
+++ Js/dinosaur.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const gameOverContainer = document.getElementById('game-over-container');
   const resetButton = document.getElementById('reset-button');
   const gameOverMessage = document.getElementById('game-over-message');
+  const scoreListContainer = document.getElementById('score-list-container');
 
   let playerBottom = 0;
   let playerLeft = 10;
@@ -84,23 +85,23 @@ document.addEventListener('DOMContentLoaded', () => {
   function generateObstacle() {
     let obstacleLeft = 600;
     let obstacleBottom = 0;
-  
+
     const obstacle = document.createElement('div');
     obstacle.classList.add('obstacle');
     obstacle.style.left = obstacleLeft + 'px';
     obstacle.style.bottom = obstacleBottom + 'px';
     obstacleContainer.appendChild(obstacle);
-  
+
     let obstacleMoveInterval = setInterval(() => {
-      if (obstacleLeft <= -25) { // 장애물이 화면을 벗어나면 스코어 증가
+      if (obstacleLeft <= -25) {
         clearInterval(obstacleMoveInterval);
         obstacleContainer.removeChild(obstacle);
         score++;
       }
-  
-      obstacleLeft -= 8; // 장애물의 이동 속도를 더 빠르게 조정
+
+      obstacleLeft -= 8;
       obstacle.style.left = obstacleLeft + 'px';
-  
+
       if (
         obstacleLeft > playerLeft - 50 &&
         obstacleLeft < playerLeft + 50 &&
@@ -115,8 +116,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }, 20);
   }
-  
-  
 
   function startGame() {
     obstacleInterval = setInterval(generateObstacle, 3000);
@@ -125,8 +124,37 @@ document.addEventListener('DOMContentLoaded', () => {
   function showGameOver() {
     gameStarted = false;
     gameOverContainer.style.display = 'block';
-    gameOverMessage.textContent = '게임 오버! 점수: ' + score;
+    gameOverMessage.textContent = 'Game Over! Score: ' + score;
+
+    // localStorage에서 기존 스코어 리스트 가져오기
+    let scores = JSON.parse(localStorage.getItem('scores')) || [];
+
+    // 현재 스코어를 리스트에 추가하기
+    scores.push(score);
+
+    // 스코어를 내림차순으로 정렬하기
+    scores.sort((a, b) => b - a);
+
+    // 스코어를 10개로 제한하기
+    scores = scores.slice(0, 10);
+
+    // 업데이트된 스코어를 localStorage에 저장하기
+    localStorage.setItem('scores', JSON.stringify(scores));
+
+    // 스코어 리스트를 화면에 표시하기
+    displayScoreList(scores);
+
     document.removeEventListener('keyup', control);
+  }
+
+  function displayScoreList(scores) {
+    scoreListContainer.innerHTML = '';
+
+    scores.forEach((score, index) => {
+      const scoreItem = document.createElement('li');
+      scoreItem.textContent = `${index + 1}. ${score}`;
+      scoreListContainer.appendChild(scoreItem);
+    });
   }
 
   function resetGame() {
@@ -140,4 +168,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   resetButton.addEventListener('click', resetGame);
+
+  // On page load, display the score list from localStorage
+  const scores = JSON.parse(localStorage.getItem('scores')) || [];
+  // 스코어 랭킹을 초기화하기 위해 localStorage에서 'scores' 키 제거
+  localStorage.removeItem('scores');
+
+  // 화면에서 스코어 리스트를 초기화하여 표시 제거
+  displayScoreList([]);
 });
