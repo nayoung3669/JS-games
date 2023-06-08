487a753975cac4f463f46e3fec613383ef99673b
nayoung3669 <skdud3669@gmail.com>
2023-06-08 14:47:29 +0900

    responsive web and ranking system but no login

--- Js/mypage.js
+++ Js/mypage.js
@@ -7,6 +7,8 @@ if (localStorage.getItem('blockgame_records')) {
     $("#blockScore").append(temp_html)
 } else {
     temp_html = `<span> No records yet.<span>`
+    $("#typingScore").append(temp_html)
+
 }
 
 if (localStorage.getItem('typinggame_records')) {
@@ -18,15 +20,19 @@ if (localStorage.getItem('typinggame_records')) {
     $("#typingScore").append(temp_html)
 } else {
     temp_html = `<span> No records yet.<span>`
+    $("#typingScore").append(temp_html)
+
 }
 
-// if (localStorage.getItem('blockgame_records')) {
-//     const scores = JSON.parse(localStorage.getItem('blockgame_records'))
-//     // var blockScore = localStorage.getItem('blockgame_records')
-//     const bestScore = scores[0].score
-//     const bestDate = scores[0].date
-//     temp_html = `<span> ${bestScore} on ${bestDate}<span>`
-//     $("#blockScore").append(temp_html)
-// } else {
-//     temp_html = `<span> No records yet.<span>`
-// }
+if (localStorage.getItem('dino_records')) {
+    const scores = JSON.parse(localStorage.getItem('dino_records'))
+    // var blockScore = localStorage.getItem('blockgame_records')
+    const bestScore = scores[0].score
+    const bestDate = scores[0].date
+    temp_html = `<span> ${bestScore} on ${bestDate}<span>`
+    $("#dinosaurScore").append(temp_html)
+} else {
+    temp_html = `<span> No records yet.<span>`
+    $("#typingScore").append(temp_html)
+
+}
